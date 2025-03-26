40a33e6349f0d78a8012dc2eed5dc3854ed08744
Mariane Joaquim Melo <marymeelo20@gmail.com>
2025-03-26 14:05:19 -0300

    aa

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import {Favorites, Profile} from "./components/gallery";
 import Test from "./components/test";
 import {View, StyleSheet} from 'react-native';
 
+// Only import react-native-gesture-handler on native platforms
+import 'react-native-gesture-handler';
+
 export default function App() {
   return (
     <View style={styles.container}>
