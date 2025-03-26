5717fa230a84370500255c018d43282d3aa7ebf3
Mariane Joaquim Melo <marymeelo20@gmail.com>
2025-03-26 17:09:08 -0300

    icon

--- App.js
+++ App.js
@@ -2,23 +2,50 @@ import 'react-native-gesture-handler';
 
 import { NavigationContainer } from '@react-navigation/native';
 
-import { createDrawerNavigator } from '@react-navigation/drawer';
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Test from './components/test';
 import Login from './components/Login';
 
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 export default function App() {
 
-const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
   return (
 
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name='Login' component={Login}/>
-        <Drawer.Screen name='Home' component={Test}/>
-      </Drawer.Navigator>
+      <BottomTab.Navigator 
+      initialRouteName='Login'
+      screenOptions={{
+        tabBarActiveTintColor:'#841584',
+        tabBarActiveBackgroundColor:'#d6b6cb',
+        tabBarInactiveBackgroundColor: '#e6e1aa',
+        tabBarInactiveTintColor: '#6e424b',
+        headerStyle:{backgroundColor: '#d6b6cb'},
+        headerTintColor: '#841584',
+        // headerTitleAlign: 'center'
+
+      }}
+      >
+        <BottomTab.Screen name='Home' component={Test}
+        options={{
+          tabBarIcon: () => (
+            <MaterialIcons name="home" size={30} color="#FF007F" />
+          ),
+        }} />
+
+        <BottomTab.Screen name='Login' component={Login}
+        options={{
+          tabBarIcon: () => (
+            <MaterialIcons name="login" size={30} color="#FF007F" />
+          ),
+        }}
+        
+        />
+        
+      </BottomTab.Navigator>
     </NavigationContainer>   
   );
 }
--- components/Login.js
+++ components/Login.js
@@ -1,11 +1,11 @@
 
 import { View, TextInput, Button,Text, StyleSheet,} from "react-native"
 
-
 export default function Login({navigation}){
     return(
         <View style={styles.container} >
             <Text>LOGIN</Text>
+
             <TextInput
             style={styles.input}
             placeholder="Nome"
